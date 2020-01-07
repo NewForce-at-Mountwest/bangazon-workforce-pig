8f9f2216acc031fc2f1ea20266a389d737b6851e
Calebooski <mrmeadows2018@gmail.com>
2020-01-07 11:39:46 -0500

    saving and checking pr request

---
+++ BangazonWorkforce/Controllers/DepartmentsController.cs
@@ -0,0 +1,284 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using BangazonWorkforce.Models;
+
+
+namespace BangazonWorkforce.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+
+        private readonly IConfiguration _config;
+
+        public DepartmentsController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET: Departments
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                     SELECT
+                    COUNT(e.FirstName) as NumberOfEmployees,
+                    d.Id,
+                     d.Name,
+                    d.Budget
+                    FROM Department d
+                    LEFT JOIN Employee e ON e.DepartmentId = d.Id
+                    GROUP by d.Name, d.Budget , d.Id";
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<Department> departments = new List<Department>();
+
+                    while (reader.Read())
+                    {
+                        Department department = new Department()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
+                            NumberOfEmployees = reader.GetInt32(reader.GetOrdinal("NumberOfEmployees"))
+                        };
+                        departments.Add(department);
+                    }
+                    reader.Close();
+                    return View(departments);
+                }
+
+            }
+        }
+
+        // GET: Departments/Details/5
+        public ActionResult Details(int id)
+        {
+
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            Id, Name, Budget
+                        FROM Department
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Department department = new Department();
+
+                    if (reader.Read())
+                    {
+                        department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+
+                        };
+                    }
+                    reader.Close();
+
+
+                    return View(department);
+                }
+            }
+        }
+
+        //GET: Departments/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departments/Create
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<ActionResult> Create(CreateStudentViewModel model)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"INSERT INTO Student
+        //        ( FirstName, LastName, SlackHandle, CohortId )
+        //        VALUES
+        //        ( @firstName, @lastName, @slackHandle, @cohortId )";
+        //            cmd.Parameters.Add(new SqlParameter("@firstName", model.student.FirstName));
+        //            cmd.Parameters.Add(new SqlParameter("@lastName", model.student.LastName));
+        //            cmd.Parameters.Add(new SqlParameter("@slackHandle", model.student.SlackHandle));
+        //            cmd.Parameters.Add(new SqlParameter("@cohortId", model.student.CohortId));
+        //            cmd.ExecuteNonQuery();
+
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //    }
+        //}
+
+        // GET: Departments/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            Id, Name, Budget
+                        FROM Department
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Department department = new Department();
+
+                    if (reader.Read())
+                    {
+                        department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+
+                        };
+                    }
+                    reader.Close();
+
+                    return View(department);
+                }
+            }
+        }
+
+        // POST: Departments/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Department department)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Department
+                                            SET Name=@Name, 
+                                            Budget=@Budget
+                                            WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@Name", department.Name));
+                        cmd.Parameters.Add(new SqlParameter("@Budget", department.Budget));
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View(department);
+            }
+        }
+
+        // GET: Departments/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT
+                            Id, Name, Budget
+                        FROM Department
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Department Department = null;
+
+                    if (reader.Read())
+                    {
+                        Department = new Department
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
+
+                        };
+                    }
+                    reader.Close();
+
+                    return View(Department);
+                }
+            }
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM StudentExercise WHERE studentId = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                    }
+                }
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Student WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+
+                    }
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
