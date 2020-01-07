942abdefc08829a3c526dc51ce476cddfe610e07
Your Name <lukemiller4779@gmail.com>
2020-01-07 10:44:21 -0500

    employee list

---
+++ BangazonWorkforce/Controllers/EmployeeController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using BangazonWorkforce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace BangazonWorkforce.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public EmployeeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    string query = @"SELECT Employee.Id AS 'Id', DepartmentId, Department.Budget AS 'Budget', Department.Id AS 'DeptId', FirstName,  LastName, isSupervisor,  Department.Name AS 'DeptName' FROM Employee JOIN Department ON DepartmentId = Department.Id ";
+                    cmd.CommandText = query;
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Employee> employees = new List<Employee>();
+                    while (reader.Read())
+                    {
+
+                        int idColumnPosition = reader.GetOrdinal("Id");
+
+                        int idValue = reader.GetInt32(idColumnPosition);
+                        int DepartmentIdColumnPosition = reader.GetOrdinal("DepartmentId");
+                        int DepartmentIdValue = reader.GetInt32(DepartmentIdColumnPosition);
+
+                        int FirstNameColumnPosition = reader.GetOrdinal("FirstName");
+                        string FirstNameValue = reader.GetString(FirstNameColumnPosition);
+                        int LastNameColumnPosition = reader.GetOrdinal("LastName");
+                        string LastNameValue = reader.GetString(LastNameColumnPosition);
+                        bool IsSupervisorValue = reader.GetBoolean(reader.GetOrdinal("IsSupervisor"));
+                        Employee employee = new Employee
+                        {
+                            Id = idValue,
+                            FirstName = FirstNameValue,
+                            LastName = LastNameValue,
+                            DepartmentId = DepartmentIdValue,
+                            IsSuperVisor = IsSupervisorValue,
+                            CurrentDepartment = new Department{
+                                Name = reader.GetString(reader.GetOrdinal("DeptName")),
+                                Budget = reader.GetInt32(reader.GetOrdinal("Budget")),
+                                Id = reader.GetInt32(reader.GetOrdinal("DeptId"))
+
+                            }
+                        };
+                        employees.Add(employee);
+                    }
+                    reader.Close();
+                    return View(employees);
+                }
+            }
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT  Employee.Id AS 'Id', FirstName, DepartmentId, LastName, Department.Name AS 'DeptName', isSupervisor 
+FROM Employee JOIN Department ON DepartmentId = Department.Id WHERE Employee.Id = @Id";
+                    cmd.Parameters.Add(new SqlParameter("@Id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    Employee employee = null;
+                    if (reader.Read())
+                    {
+                        employee = new Employee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                            IsSuperVisor = reader.GetBoolean(reader.GetOrdinal("isSupervisor")),
+                            CurrentDepartment = new Department
+                            {
+                                Name = reader.GetString(reader.GetOrdinal("DeptName"))
+                            }
+                        };
+                    }
+                    reader.Close();
+
+                    return View(employee);
+                }
+            }
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
