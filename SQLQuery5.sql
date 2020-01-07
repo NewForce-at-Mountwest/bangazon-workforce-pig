68bf31ebd238a92a9071831a15cb1a2662b72922
Your Name <lukemiller4779@gmail.com>
2020-01-07 11:05:42 -0500

    need to import my index from another branch

---
+++ SQLQuery5.sql
@@ -0,0 +1,5 @@
+﻿SELECT  Employee.Id AS 'Id', FirstName, 
+LastName, Department.Name AS 'DeptName', isSupervisor ,  Computer.Make, Computer.Manufacturer
+FROM Employee  
+ JOIN Department ON DepartmentId = Department.Id Join ComputerEmployee ON Employee.Id = EmployeeId JOIN Computer ON Computer.Id = ComputerId
+
